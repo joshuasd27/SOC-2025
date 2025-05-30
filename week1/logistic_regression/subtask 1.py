9eed82095d577398334054620b38f8be57b068e0
HenryCabbot <joshuasd27@gmail.com>
2025-05-30 13:06:39 +0530

    Added decision boundary visualiser for logistic regression

--- week1/logistic_regression/subtask 1.py
+++ week1/logistic_regression/subtask 1.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 
 #columns are x,y,classification class (0 or 1)
 train_data = np.genfromtxt("train.csv", delimiter=',', skip_header=1)
@@ -12,7 +14,7 @@ test_output = test_data[:,2].reshape(1,-1) # shape is 1,number of datapoints
 # X:- (n,m) m datapoints, n features
 # w:- (n,1) weights
 #y:- (1,m) outputs
-def logistic_regression(X,y,w=0,b_initial=0,learning_rate=0.0001, max_iters=5000):
+def logistic_regression(X,y,w=0,b_initial=0,learning_rate=0.0001, max_iters=10000):
     (n,m) = X.shape
     if w==0:
         w=np.zeros((n,1))
@@ -60,7 +62,7 @@ y_square = test_input[1]**2
 quadratic_test_input =  np.vstack((test_input,x_square,xy,y_square))
 w,b = logistic_regression(quadratic_train_input,train_output)
 
-#find cost
+#predict test output
 y=test_output
 z = w.T@quadratic_test_input +b
 yPredicted= 1/(1+np.exp(-z))
@@ -72,5 +74,36 @@ print(f'Percentage of 1 classified correctly : {(y.astype(bool)&yPredicted.astyp
 print(f'Percentage of 0 classified correctly : {((~y.astype(bool))&(~yPredicted.astype(bool))).sum()/(y.shape[1]-y.sum()):.2%}')
 print(f'Total percentage classified correctly : {(y==yPredicted).sum()/y.shape[1]:.2%}')
 
+#VISUALISER
+#Plot actual points
+plt.scatter(test_input[0],test_input[1], c=test_output, cmap='bwr', edgecolors='b')
 
+# Create custom legend handles
+class_0_patch = mpatches.Patch(color='blue', label='Class 0')
+class_1_patch = mpatches.Patch(color='red', label='Class 1')
+
+# Add legend to plot
+plt.legend(handles=[class_0_patch, class_1_patch], title="Classes")
+
+
+#heatmap of probability over input space
+x_min, x_max = test_input[0].min()-1, test_input[0].max()+1
+y_min, y_max = test_input[1].min()-1, test_input[1].max()+1
+xx,yy = np.meshgrid(np.linspace(x_min,x_max,1000), np.linspace(y_min,y_max,1000))
+grid_predict = np.vstack((xx.ravel(),yy.ravel()))
+
+#predict grid output
+x_square = grid_predict[0]**2
+xy = grid_predict[0]*grid_predict[1]
+y_square = grid_predict[1]**2
+quadratic_grid_predict =  np.vstack((grid_predict,x_square,xy,y_square))
+
+y=test_output
+z = w.T@quadratic_grid_predict +b
+yPredicted= 1/(1+np.exp(-z))
+yPredicted= (yPredicted>0.5).astype(int)
+
+grid_predict = yPredicted.reshape(xx.shape)
+plt.contourf(xx, yy, grid_predict, 50, cmap='bwr', alpha=0.2)
+plt.show()
 
--- week1/logistic_regression/subtask2.py
+++ week1/logistic_regression/subtask2.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 
 #columns are x,y,classification class (0 or 1)
 train_data = np.genfromtxt("train2.csv", delimiter=',', names=True, dtype=None, encoding=None)
@@ -13,7 +15,7 @@ map_func = np.vectorize(color_dict.get)
 #train_output_num = map_func(train_output).reshape(1,-1)
 
 
-def logistic_regression(X,y,w=0,b_initial=0,learning_rate=0.0001, max_iters=1000):
+def logistic_regression(X,y,w=0,b_initial=0,learning_rate=0.5, max_iters=3000):
     (n,m) = X.shape
     if w==0:
         w=np.zeros((n,1))
@@ -31,19 +33,19 @@ def logistic_regression(X,y,w=0,b_initial=0,learning_rate=0.0001, max_iters=1000
         b -= learning_rate*db
 
 
-        # if iter%int(max_iters/5)==0:
-        #     cost =0
-        #     for i in range(m):
-        #         if y[0][i] ==1:
-        #             loss = -np.log(yPredicted[0][i])
-        #         else:
-        #             loss= -np.log(1-yPredicted[0][i])
-        #         cost+=loss
-        #     cost /=m
+        if iter%int(max_iters/5)==0:
+            cost =0
+            for i in range(m):
+                if y[0][i] ==1:
+                    loss = -np.log(yPredicted[0][i])
+                else:
+                    loss= -np.log(1-yPredicted[0][i])
+                cost+=loss
+            cost /=m
 
 
-        #     print(cost)
-    #print(w)
+            print(cost)
+    print(w)
     return w,b
 
 
@@ -52,6 +54,8 @@ b_list = []
 for i,color in enumerate(unique):
     y= (train_output==color).astype(float).reshape(1,-1)
     w_colori,b_colori =logistic_regression(train_input,y)
+    print(train_input.shape)
+    print(y.shape)
     w_colori= w_colori.reshape(-1,1)
     w_list.append(w_colori)
     b_list.append(b_colori)
@@ -71,14 +75,32 @@ for i,color in enumerate(unique):
 yPredicted_arr = np.array(yPredicted_list)
 prediction = np.array([np.argmax(yPredicted_arr[:,0,i]) for i in range(test_input.shape[1])])
 prediction = np.array( [unique[arg] for i,arg in enumerate(prediction)])
-print(type(test_output), test_output.dtype, test_output.shape)
-print(type(prediction), prediction.dtype, prediction.shape)
+
 
 percent_correct = np.mean(test_output==prediction)
 
 print(f'Distribution of data points')
 print(frequency)
-print(f'Percentage classified correctly for color:- {percent_correct:.2%}')
+print(f'Percentage classified correctly :- {percent_correct:.2%}')
+
+
+#VISUALISER
+#Plot actual points
+plt.scatter(test_input[0],test_input[1], c=test_output, cmap='bwr', edgecolors='b', s=50)
+
+
+#decision boundaries of each color
+x_min, x_max = test_input[0].min()-1, test_input[0].max()+1
+y_min, y_max = test_input[1].min()-1, test_input[1].max()+1
+for i,color in enumerate(unique):
+    #get y points o decision boundary, ST z = w_list[i].T@test_input +b_list[i] = 0
+    X = np.linspace(x_min,x_max,200)
+    Y = -(w_list[i][0]*X +b_list[i]) / w_list[i][1]
+    Y=Y.ravel()
+    plt.plot(X,Y, c=color)
+plt.show()
+
+
 
 
 
